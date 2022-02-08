b1b0828d08d4984caa161fbbc188e8eaf77a9ad1
eymenyaci <eymenyaci@gmail.com>
2022-02-08 21:11:34 +0300

    operators commit

---
+++ operators/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int x = 3;
+           int y = 2;
+           //y değerine x ekleniyor.
+           y+=x;
+           Console.WriteLine(y);
+           
+           bool aktiflik = true;
+           bool stokDurumu = false;
+
+           // ve mantıksal operatörü
+           if (aktiflik && stokDurumu)
+           {
+               Console.WriteLine("Mükemmel!");
+           }
+           else{
+               Console.WriteLine("Her iki koşul sağlanmıyor.");
+           }
+            // veya mantıksal operatörü
+           if (aktiflik || stokDurumu)
+           {
+               Console.WriteLine("Mükemmel!");
+           }
+           else{
+               Console.WriteLine("Her iki koşul sağlanmıyor.");
+           }
+            // ve mantıksal operatörü
+           if (aktiflik != stokDurumu)
+           {
+               Console.WriteLine("Durumlar aynı değil!");
+           }
+           else{
+               Console.WriteLine("Durumlar Aynı!");
+           }
+
+
+
+           
+
+        }
+    }
+}
--- variable/Program.cs
+++ variable/Program.cs
@@ -6,17 +6,22 @@ namespace variable
     {
         static void Main(string[] arg)
         {
-            //Tam Sayı Değişken
-            int sayi=5;
-            //Ondalıklı Değişken
-            double sayi1=5.2;
-            //Metin Veri Tipi
-            string metin="Metin Veri Tipi";
-            //karakter Veri Tipi
-            char karakter = 'a';
-            //Doğrusal Veri Tipi
-            Boolean aktiflik = true;
-        
+            // //Tam Sayı Değişken
+            // int sayi=5;
+            // //Ondalıklı Değişken
+            // double sayi1=5.2;
+            // //Metin Veri Tipi
+            // string metin="Metin Veri Tipi";
+            // //karakter Veri Tipi
+            // char karakter = 'a';
+            // //Doğrusal Veri Tipi
+            // Boolean aktiflik = true;
+
+            int degisken=5;
+            string degisken="Merhaba";
+            Console.WriteLine(degisken);
+            
+                    
 
 
             
