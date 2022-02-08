ed7dd88a75f407483a95e53598ab7c52ba9faa61
eymenyaci <eymenyaci@gmail.com>
2022-02-08 22:55:43 +0300

    tryCatchFinally commit

---
+++ try-catch-finally/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace tryCatchFinally
+{
+    class Program
+    {
+
+    
+         public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz : ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Sayınız : " + sayi);
+            }
+            catch(Exception hata)
+            {
+                Console.WriteLine("Bir hata oluştu ! " + hata.Message );
+            }
+             
+        }
+    }
+}
