d424734a45688a63aa857227f1bad12a4afc5e7a
eymenyaci <eymenyaci@gmail.com>
2022-02-08 20:48:43 +0300

    variable commit

---
+++ variable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace variable
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            //Tam Sayı Değişken
+            int sayi=5;
+            //Ondalıklı Değişken
+            double sayi1=5.2;
+            //Metin Veri Tipi
+            string metin="Metin Veri Tipi";
+            //karakter Veri Tipi
+            char karakter = 'a';
+            //Doğrusal Veri Tipi
+            Boolean aktiflik = true;
+        
+
+
+            
+        }
+    }
+}
