44765f0aafddf05fefe365b3a850ef1dc3cebbd7
eymenyaci <eymenyaci@gmail.com>
2022-02-12 22:17:57 +0300

    question-2

--- questions/question-2/Program.cs
+++ questions/question-2/Program.cs
@@ -1,28 +1,76 @@
 ﻿using System;
 
 namespace question2
-{   
+{
     class Solution
     {
-       
+
+
         public static void Main(string[] args)
         {
             // Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n, m). 
             // Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. 
             // Kullanıcının girmiş olduğu sayılardan m'e eşit yada tam bölünenleri console'a yazdırın.
-            Console.WriteLine("please add first and second number : ");
-            int n1=Convert.ToInt32(Console.ReadLine());
-            int n2=Convert.ToInt32(Console.ReadLine());
-            
-            
-            
-            
-                      
+            List<int> numbers = new List<int>();
+            int counter=0;
+            while (true)
+            {
+                Console.WriteLine("please add positive numbers : ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int m = Convert.ToInt32(Console.ReadLine());
+                if (n > 0 && m > 0)
+                {
+                    Console.WriteLine("Calculating...");
+                    while(true)
+                    {
+                        
+                        int entered = Convert.ToInt32(Console.ReadLine());
+                        counter++;
+                        
+                        if (entered == m || m%entered==0)
+                        {
+                            numbers.Add(entered);
+                            
+                            
+                        }
+                        if (counter==n)
+                        {
+                            break;
+                        }
+                        
+                        
+
+                    }
+                    break;
+                }
+            }
+            foreach (var item in numbers)
+            {
+                Console.Write(item+" ");
+                
+            }
+
+            // for (int i = 0; i < n; i++)
+            // {
+            //     
+            //     if (entered == m || m % entered == 0)
+            //     {
+            //         numbers.Add(entered);
+            //     }
+
+            // }
+
+
+
+
+
+
+
 
         }
-        
-       
+
+
 
     }
-    
+
 }
