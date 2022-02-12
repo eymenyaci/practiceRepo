83cec5da386e08828f5545a384860d513a23737e
eymenyaci <eymenyaci@gmail.com>
2022-02-12 18:28:57 +0300

    methods commit

---
+++ methods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace methods
+{
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+           int result = sub(1,2);
+           methods mtd = new methods();
+           mtd.printTerminal(Convert.ToString(result));
+           
+
+        }
+        static int sub(int x,int y)
+        {
+            return (x+y);
+        }
+
+    }
+
+    class methods
+    {
+        public void printTerminal(string input)
+        {
+            Console.WriteLine(input);
+        }
+    }
+
+}
