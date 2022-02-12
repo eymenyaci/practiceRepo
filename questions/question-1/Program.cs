37cc97abd93a1fa8e34414b8b846c20879dd8b98
eymenyaci <eymenyaci@gmail.com>
2022-02-12 21:29:21 +0300

    question-1 commit

---
+++ questions/question-1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace question1
+{
+   
+    
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            // Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n). Sonrasında
+            // kullanıcıdan n adet pozitif sayı girmesini isteyin. Kullanıcının 
+            // girmiş olduğu sayılardan çift olanlar console'a yazdırın.
+            
+            Console.WriteLine("Please enter how many numbers in : ");
+            int v = Convert.ToInt32(Console.ReadLine());
+            int entered = enteredUnit(v);
+            List<int> evenList = new List<int>();
+            for (int i = 0; i < entered; i++)
+            {   
+                Console.WriteLine("Please Posivite number add : ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                if (n%2==0)
+                {
+                   evenList.Add(n);
+                }
+
+            }
+            foreach (var item in evenList)
+            {
+                Console.Write(item+" ");
+            }
+            
+                      
+
+        }
+        public static int enteredUnit(int n)
+        {
+           
+            while (n<1)
+            {
+                Console.WriteLine("Again enter a positive number :");
+                n = Convert.ToInt32(Console.ReadLine());
+                
+            }
+            Console.WriteLine(n+" to be entered.");
+            return n;
+            
+        }
+       
+
+    }
+    
+}
---
+++ questions/question-2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace question2
+{   
+    class Solution
+    {
+       
+        public static void Main(string[] args)
+        {
+            // Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n, m). 
+            // Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. 
+            // Kullanıcının girmiş olduğu sayılardan m'e eşit yada tam bölünenleri console'a yazdırın.
+            Console.WriteLine("please add first and second number : ");
+            int n1=Convert.ToInt32(Console.ReadLine());
+            int n2=Convert.ToInt32(Console.ReadLine());
+            
+            
+            
+            
+                      
+
+        }
+        
+       
+
+    }
+    
+}
