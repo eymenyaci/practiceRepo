f114985c5fcd11efc19129bbee4050954aec6f66
eymenyaci <eymenyaci@gmail.com>
2022-02-13 16:23:17 +0300

    question-4

---
+++ questions/question-4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace question4
+{
+    class Solution
+    {
+        public static void Main(String[] args)
+        {
+            // Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin. 
+            // Cümledeki toplam kelime ve harf sayısını console'a yazdırın.
+            Console.WriteLine("Please text..");
+            string text = Console.ReadLine();
+            int length = text.Length;
+            Console.ReadKey();
+            int hUnit = 0;
+            int word = 1;
+            
+            for (int i = 0; i < length; i++)
+            {
+                if (text[i]!=' ')
+                {
+                    hUnit++;
+
+                }
+                else if (text[i]==' ')
+                {
+                    word++;
+                }
+                
+            }
+            Console.WriteLine("Bu cümlede "+hUnit+" harf "+word+" kelime vardır.");
+                
+            
+            
+        }
+    }
+}
